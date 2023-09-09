b0da2a4e9adf934ec4a4def0248db74950cf67e7
Vishal Majumdar <vishalmajumdar1@gmail.com>
2023-09-10 01:58:59 +0530

    Build Header
    Made the header component with 2 buttons and site title in the middle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="p-4 shadow-sm">
+      <nav className="flex justify-between items-center">
+        <button className="bg-[#209cee] px-4 py-2 rounded-lg text-white font-bold hover:bg-[#0065a8]  transition ease-in-out delay-150 duration-300">
+          <a
+            href="https://github.com/vishalmajumdar/random-facts"
+            target="_blank"
+          >
+            GitHub 🔗
+          </a>
+        </button>
+        <h1 className="text-xl font-bold text-[#209cee]">Random Facts</h1>
+        <button className="bg-[#209cee] px-4 py-2 rounded-lg text-white font-bold cursor-default hover:bg-[#0065a8] transition ease-in-out delay-150 duration-300">
+          Made with ❣️
+        </button>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
