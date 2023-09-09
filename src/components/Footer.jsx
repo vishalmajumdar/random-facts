0834958a0e9cbae1a6e8d3efc9951cc67af6cbd0
Vishal Majumdar <vishalmajumdar1@gmail.com>
2023-09-10 02:31:03 +0530

    Add Footer
    Made the basic footer component and it's styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="fixed p-4 bottom-0 left-1 border-t-2 w-full text-gray-800">
+      <p className="text-center font-medium">Copyright ©️ 2023</p>
+    </footer>
+  );
 };
 
 export default Footer;
