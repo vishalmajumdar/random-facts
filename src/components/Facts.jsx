c35dcfce8ddcb911f56495310a6d17df1c1ddda6
Vishal Majumdar <vishalmajumdar1@gmail.com>
2023-09-16 00:40:14 +0530

    Add state variable
    The state variable for fact has been assigned a string which will store the useless fact retrieved from external API and then displayed to the screen

--- src/components/Facts.jsx
+++ src/components/Facts.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
   .then((res) => res.json())
@@ -6,13 +7,14 @@ fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
   .catch((err) => console.log(err));
 
 const Facts = () => {
+  const [fact, setFact] = useState(
+    "The button below generates useless facts. Give it a try..."
+  );
   return (
     <>
       <section className=" w-[90%] md:w-[75%] bg-slate-200 rounded-lg p-8">
         <p className="text-gray-800 leading-6 text-center overflow-hidden">
-          The longest word in the English language, according to the Oxford
-          English Dictionary, is:
-          pneumonoultramicroscopicsilicovolcanoconiosis..
+          {fact}
         </p>
       </section>
       <button className="bg-[#209cee] mt-4 px-4 py-2 rounded-lg text-white font-bold hover:bg-[#0065a8] transition ease-in-out delay-150 duration-300">
