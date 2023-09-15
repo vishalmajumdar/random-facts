e40c15fe7b1825c0297eda4f1e256935ac092c10
Vishal Majumdar <vishalmajumdar1@gmail.com>
2023-09-16 01:07:01 +0530

    Add getFact function
    a function has been defined to get data from external API and store in the useState variable so that everytime the button is clicked the UI will get render again, also the code has been refactore a litte bit and 2 comments were added too

--- src/components/Facts.jsx
+++ src/components/Facts.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { useState } from "react";
 
-fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
-  .then((res) => res.json())
-  .then((data) => console.log(data.text))
-  .catch((err) => console.log(err));
-
 const Facts = () => {
   const [fact, setFact] = useState(
     "The button below generates useless facts. Give it a try..."
-  );
+  ); // variable to store fact
+
+  function getFact() {
+    fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
+      .then((res) => res.json())
+      .then((data) => setFact(data.text))
+      .catch((err) => console.log(err));
+  } // function to get fact from external API and store in the variable
+
   return (
     <>
       <section className=" w-[90%] md:w-[75%] bg-slate-200 rounded-lg p-8">
@@ -17,7 +20,10 @@ const Facts = () => {
           {fact}
         </p>
       </section>
-      <button className="bg-[#209cee] mt-4 px-4 py-2 rounded-lg text-white font-bold hover:bg-[#0065a8] transition ease-in-out delay-150 duration-300">
+      <button
+        className="bg-[#209cee] mt-4 px-4 py-2 rounded-lg text-white font-bold hover:bg-[#0065a8] transition ease-in-out delay-150 duration-300"
+        onClick={getFact}
+      >
         Generate
       </button>
     </>
